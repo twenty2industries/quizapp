f085d96f99188863455f7537cbf960d8e98c7479
twenty2industries <burak.bostanli.1@web.de>
2025-04-07 23:36:28 +0200

    first JSON has been created

---
+++ script.js
@@ -0,0 +1,11 @@
+let questions = [
+  {
+    question: "Wer hat HTML erfunden",
+    answer_1: "Robbie 'Williams",
+    answer_2: "Lady Gaga",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+  },
+];
+
